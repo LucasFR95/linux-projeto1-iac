5c7898ad9a0cf0e9543432ed192522cee846ffb9
LucasFR95 <lucasfr@outlook.com.br>
2023-02-27 21:12:56 -0300

    Arquivo IaC v.1

---
+++ script.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+echo "Criando diretorios ..."
+
+mkdir /publico
+
+mkdir /adm
+
+mkdir /ven
+
+mkdir /sec
+
+echo "Criando grupos de usuarios... "
+
+groupadd  GRP_ADM
+
+groupadd GRP_VEN
+
+groupadd GRP_SEC
+
+echo "Criando usuarios..."
+
+useradd carlos -c "Carlos" -s bin/bash -m -p $(openssl passwd -crypt 1234) -G GRP_ADM
+
+useradd maria -c "Maria" -s bin/bash -m -p $(openssl passwd -crypt 1234) -G GRP_ADM
+
+useradd joao -c "Joao" -s bin/bash -m -p $(openssl passwd -crypt 1234) -G GRP_ADM
+
+useradd debora -c "Debora" -s bin/bash -m -p $(openssl passwd -crypt 1234) -G GRP_VEN
+
+useradd sebastiana -c "Sebastiana" -s bin/bash -m -p $(openssl passwd -crypt 1234) -G GRP_VEN
+
+useradd roberto -c "Roberto" -s bin/bash -m -p $(openssl passwd -crypt 1234) -G GRP_VEN
+
+useradd josefina -c "Josefina" -s bin/bash -m -p $(openssl passwd -crypt 1234) -G GRP_SEC
+
+useradd amanda -c "Amanda" -s bin/bash -m -p $(openssl passwd -crypt 1234) -G GRP_SEC
+
+useradd rogerio -c "Rogerio" -s bin/bash -m -p $(openssl passwd -crypt 1234) -G GRP_SEC
+
+echo "Especificando permissões dos diretorios... "
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /publico
+
+echo "Finalizado"
+
